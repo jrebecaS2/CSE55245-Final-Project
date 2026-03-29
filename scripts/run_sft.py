edb89cf40334221d93c309a31fe2498e1fea38a3
jrebecaS2 <kohler.julinha@gmail.com>
2026-03-29 19:07:14 -0400

    Remove model checkpoint from git, add to .gitignore

--- scripts/run_sft.py
+++ scripts/run_sft.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from train_sft import SFTTrainer
